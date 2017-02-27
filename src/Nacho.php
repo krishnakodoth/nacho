bb2103cd44211013ec833e1643ce1d093fb70676
Krishnaprasad K <krishnanova@gmail.com>
2017-02-27 17:41:33 +0530

    Add files via upload

---
+++ src/Nacho.php
@@ -0,0 +1,10 @@
+<?php namespace PhilipBrown\Nacho;
+ 
+ class Nacho {
+ 
+  public function hasCheese($bool = true)
+  {
+    return $bool;
+  }
+ 
+}
---
+++ tests/NachoTest.php
@@ -0,0 +1,13 @@
+<?php
+ 
+use PhilipBrown\Nacho\Nacho;
+ 
+class NachoTest extends PHPUnit_Framework_TestCase {
+ 
+  public function testNachHasCheese()
+  {
+    $nacho = new Nacho;
+    $this->assertTrue($nacho->hasCheese());
+  }
+ 
+}
